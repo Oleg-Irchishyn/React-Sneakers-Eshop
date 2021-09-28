cdbfc21528a6bf1ecc452d87b654af249c166eb8
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2021-09-28 10:59:28 +0300

    updating project

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/index.scss';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
